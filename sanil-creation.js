d81279351f8865fcc222a0cbcf46189783201e27
3bfc <bestfriendscreate2@gmail.com>
2024-05-14 09:40:55 -0400

    added in older code

--- sanil-creation.js
+++ sanil-creation.js
@@ -21,6 +21,7 @@ var sanil = (function($) {
     // Initialize your library with options
     if (options.collective) {
       set_collective(options.collective);
+      update_package_display();
       initialize_listeners();
     }
     console.log("Initializing sanil with options:", options);
@@ -33,24 +34,63 @@ var sanil = (function($) {
   }
 
   function initialize_listeners() {
-    update_package_display();
-
+  
     // listens to changes on the radio cards
     $packageRadios.on('change',function() {
       update_package_display();
       var new_price_id = $(this).val();
       $submitBtn.attr('data-ms-price:add', new_price_id);
-      alert(new_price_id)
     })
+
+    $form.attr('action','#');
+    $form.submit(function(event){
+      event.preventDefault;
+    })
+    $form.submit(async function(event){
+      event.preventDefault;
+      try {
+        var formData = $(this).serializeArray();
+        var email = formData.find(item => item.name === 'email').value;
+        var password = formData.find(item => item.name === 'password').value;
+        var first = formData.find(item => item.name === 'first').value;
+        var last = formData.find(item => item.name === 'last').value;
+        var sport = formData.find(item => item.name === 'sport').value;
+        var selectedValue = $packageRadios.filter(':checked').val();
+
+        // Signup member
+        await memberstack.signupMemberEmailPassword({
+            customFields: {
+                'plan-intent': $planIntent.val(),
+                'sport-package': sport,
+                'first-name': first,
+                'last-name': last,
+            },
+            email: email,
+            password: password
+        });
+
+        // Purchase plans with checkout
+        var domain = "https://" + window.location.hostname;
+        await memberstack.purchasePlansWithCheckout({
+            priceId: selectedValue,
+            metadataForCheckout: {
+                SPORT: sport,
+                COLLECTIVE: collective
+            },
+            cancelUrl: domain,
+            successUrl: domain,
+            autoRedirect: true
+        });
+    } catch (err) {
+      displayErrorMessage(err.message);
+    }
+    });
   }
 
   function set_collective(value) {
     collective = value;
   }
   return {
-      init: init,
-      doSomething: function() {
-          console.log("Doing something...");
-      }
+      init: init
   };
 })(jQuery);
