ae987ca1ab160fbfae6b66d28987839e97b9ce18
3bfc <bestfriendscreate2@gmail.com>
2024-05-14 23:14:43 -0400

    added in member-otc

--- member-otc.js
+++ member-otc.js
@@ -1,21 +1,18 @@
-$(document).ready(function() {
-  // Track changes in the radio group
-//        $('#prc_monthly-i3f609sy').prop("checked", true);
-
+$(document).ready(function () {
   var attrValue = $('input[name="package"]:checked').attr("package-name");
   $('#package-label').text(attrValue);
   $('#plan-intent').val(attrValue);
   var selectedValue = $('input[name="package"]:checked').val();
   $('#submit').attr('data-ms-price:add', selectedValue);
-   $('input[name="package"]').on('change', function() {
-     var attrValue = $('input[name="package"]:checked').attr("package-name");
+  $('input[name="package"]').on('change', function () {
+    var attrValue = $('input[name="package"]:checked').attr("package-name");
     $('#package-label').text(attrValue);
     console.log(attrValue);
   });
-  $('#otc-link').on('click',function(e){
+  $('#otc-link').on('click', function (e) {
     var selectedValue = $('input[name="package"]:checked').val();
     var sport = $('#field').val();
-    $('#otc-link').attr('href',selectedValue);
+    $('#otc-link').attr('href', selectedValue);
   });
 
 });
